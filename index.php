1539faf825ab7e8fc70f2fbc005a64e3ae2aae87
xerses7 <xerses7@gmail.com>
2012-11-08 22:18:25 +0100

    file index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	require_once("./Classes/post.php");
+	require_once("./Classes/posts.php");
+	require_once("./Classes/categories.php");
+	require_once("./Classes/comments.php");
+	require_once("./Classes/tags.php");
+	require_once("./Classes/date.php");
+	require_once("./Classes/user.php");
+	
+	echo "Completato";
+	echo "prova di errore";
+?>
