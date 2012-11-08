56a627d928d127f22f2d72125c17e63aa0e9855a
xerses7 <xerses7@gmail.com>
2012-11-08 22:43:05 +0100

    Stile e View

---
+++ View/layout.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html> 
+<html>
+<head>
+	<title>Casa dolce casa</title>
+	<meta charset="utf-8">
+	<meta name="description" content="The head tag contains the title and meta tags - important to the search engines, and information for the browser to properly display the page.">
+	<link rel=stylesheet type=txt/css href="../Stile/stile.css" />
+</head>
+</html>
