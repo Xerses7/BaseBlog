2d45e53d64e3d375738decc0889d2604c144faaf
xerses7 <xerses7@gmail.com>
2012-11-09 22:36:52 +0100

    connection update 1

--- Classes/connection.php
+++ Classes/connection.php
@@ -3,8 +3,9 @@
 	class Connection {
 		public function __construct(){
 			try{
-				$conn = new PDO('mysql:host=localhost',"dbname=$database", $username,$password );
+				$conn = new PDO("mysql:host=localhost;dbname=$database", $username,$password );
 				$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+				$conn->setFetchMode(PDO::FETCH_ASSOC);
 				return $conn;
 			} catch(PDOException $e){
 				echo "Error: " . $e->getMessage();
