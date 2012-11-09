95dc66e0341b4b09fe21d117ea80a27a3a4e313b
xerses7 <xerses7@gmail.com>
2012-11-09 22:40:42 +0100

    layout updata

--- View/layout.php
+++ View/layout.php
@@ -6,4 +6,14 @@
 	<meta name="description" content="The head tag contains the title and meta tags - important to the search engines, and information for the browser to properly display the page.">
 	<link rel=stylesheet type=txt/css href="../Stile/stile.css" />
 </head>
+
+<body>
+	<h1>La mia home</h1>
+	<?php
+	
+		include("");		
+	
+	?>
+	
+</body>
 </html>
