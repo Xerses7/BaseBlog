f00c1a3936bf63a7775c76eb858a4ce5d5b60ed5
xerses7 <xerses7@gmail.com>
2012-11-10 12:34:44 +0100

    added index view control

--- Classes/view.php
+++ Classes/view.php
@@ -2,12 +2,16 @@
 
 	class View {
 		public static function get ($path, $data = null){
-			
 			if ($data) {
 				extract($data);
 			}
-			include("../View/{$path}.view.php");
-			
+			if ($path == "index"){
+				$path = $path . ".view.php";
+				include("./View/layout.php");
+			} else {
+				$path = $path . ".view.php";
+				include("../View/layout.php");
+			}
 		}
 	}
 
