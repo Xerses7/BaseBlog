af1579f42fb1a8ee5e87871be4f4ef639876a253
xerses7 <xerses7@gmail.com>
2012-11-10 12:20:35 +0100

    update index.php with view function

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
 	require("./Classes/config.php");
-	require_once("./Classes/post.php");
-	require_once("./Classes/posts.php");
-	require_once("./Classes/categories.php");
-	require_once("./Classes/comments.php");
-	require_once("./Classes/tags.php");
-	require_once("./Classes/date.php");
-	require_once("./Classes/user.php");
 	
-	$view = "./View/index.view.php";
+	View::get("index");
 
-
-	include "View/layout.php";
 ?>
