ad0c90bf5bbb1e5927fad4585c3c1fcbc523d4db
xerses7 <xerses7@gmail.com>
2012-11-11 16:29:14 +0100

    create post controller

--- Controllers/createPost.php
+++ Controllers/createPost.php
@@ -1,7 +1,29 @@
 <?php
-	require("../Classes/config.php");
+	require("../Classes/classes.php");
 	
-	View::get("createPost");
+	$data = array();
 	
-
+	if ($_SERVER['REQUEST_METHOD'] === "POST"){
+		echo("Tentativo di inserimento...");
+		if (empty($_POST['titolo']) || empty($_POST['testo'])){
+			$data['status'] = "Riempire entrambi i form per favore !";
+		} else {
+			// get data from server
+			$data[':id_post'] = " ";
+			$data[':titolo'] = $_POST['titolo'];
+			$data[':testo'] = $_POST['testo'];
+			//TODO
+			$data[':data_ora'] = "12";
+			$data[':categoria_id'] = "1";
+			//$utente[':utente_id']
+			
+			// inserire il post nel DB
+			$dbPost = new Post();
+			$dbPost->create($data);
+			$data['status'] = "Post Inserito";
+		}
+	} 
+	
+	View::get("createPost", $data);
+	
 ?>
