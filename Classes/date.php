5fcab3f67576b8eb77c41830dea443c7f0a99f6a
xerses7 <xerses7@gmail.com>
2012-11-24 14:15:12 +0100

    date class

--- Classes/date.php
+++ Classes/date.php
@@ -1,12 +1,14 @@
 <?php
 
 	class Date {
-		public static function mySQLtoPHP () {
-		
+		public static function mySQLtoPHP ( $datetime ) {
+			$phpdate = strtotime( $mysqldate );
+			return( $phpdate );
 		}
 		
-		public static function phpToMySQL () {
-		
+		public static function phpToMySQL ( $phpdate ) {
+			$mysqldate = date( 'Y-m-d H:i:s', $phpdate );
+			return ( $mysqldate );
 		}
 	}
 
