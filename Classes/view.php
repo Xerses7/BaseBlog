7c5374119443c2a2b164ee8634efd4e36854856f
xerses7 <xerses7@gmail.com>
2013-07-14 21:24:54 +0200

    added new view paths

--- Classes/view.php
+++ Classes/view.php
@@ -7,19 +7,23 @@
 			}
 			if ($path == "index"){
 				$path = $path . ".view.php";
-				$style = "./Stile/stile.css";
+				$style = "./Stile/";
+				$highlight = "./Stile/highlight.js/";
 				$categoryContr = "./Controllers/postsByCategory.php";
 				$tagsContr = "./Controllers/postsByTag.php";
 				$home = "./index.php";
 				$login = "./View/login.view.php";
+				$js = "./Js/";
 				include("./View/layout.php");
 			} else {
 				$path = $path . ".view.php";
-				$style = "../Stile/stile.css";
+				$style = "../Stile/";
+				$highlight = "../Stile/highlight.js/";
 				$categoryContr = "../Controllers/postsByCategory.php";
 				$tagsContr = "../Controllers/postsByTag.php";
 				$home = "../index.php";
 				$login = "../View/login.view.php";
+				$js = "../Js/";
 				include("../View/layout.php");
 			}
 		}
